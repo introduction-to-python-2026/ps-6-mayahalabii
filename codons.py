376c9db1ea255e555f3575e8eaff10c16e87da01
mayahalabii <p9sgg4q22b@privaterelay.appleid.com>
2025-11-30 15:43:40 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,16 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+   path = file_path # Use the file_path argument
+   file = open(path)
+   rows = file.readlines()
+   file.close()
+
+   map = {}
+   for row in rows:
+      cells = row.strip().split('\t')
+      codon = cells [0]
+      amino_acid = cells [2]
+      map[codon]= amino_acid
+
+   return map
 
 
